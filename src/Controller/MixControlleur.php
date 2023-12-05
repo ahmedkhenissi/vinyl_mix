39df9629926f81a0e15006ffa99351e588634682
Ahmed Khenissi <Khenissiahmed3@gmail.com>
2023-12-05 15:47:26 +0000

    Doctrine Extensions: Timestampable

--- src/Controller/MixControlleur.php
+++ src/Controller/MixControlleur.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MixController extends AbstractController
+class MixControlleur extends AbstractController
 {
     #[Route('/mix/new')]
     public function new(EntityManagerInterface $entityManager): Response
